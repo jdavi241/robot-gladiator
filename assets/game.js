66a8b74f28a77032b7814ce5ef85f6eb7b73dfea
jdavi241 <davisjimm25@gmail.com>
2022-04-27 18:44:35 -0500

    fight has begun message

--- assets/game.js
+++ assets/game.js
@@ -1,7 +1,11 @@
 // this creates a function named "fight"
 
+var playerName = window.prompt("What is your robot's name?");
+var name = "your name";
+console.log("Our robot's name is " + playerName);
+console.log(playerName);
 function fight() {
     window.alert("The fight has begun!");
 }
 
-fight();
+//fight();
