4e5dab65a46004fd3afe6dd2d99eb29d32b6cd8e
jdavi241 <davisjimm25@gmail.com>
2022-05-17 15:01:27 -0500

    updated skip funciton

--- assets/game.js
+++ assets/game.js
@@ -47,6 +47,7 @@ var fight = function() {
             window.alert(playerName + " has decided to skip the fight. Goodbye!");
             //subtract money from playerMoney for skipping 
             playerMoney = playerMoney - 2;
+            console.log(playerMoney);
         } 
         // if no (false), ask question again by running fight() agian 
         else {
@@ -55,37 +56,6 @@ var fight = function() {
     } 
     else {
         window.alert("You need to choose a valid option. Try agian!");
-    }
-    
-    // Subtract the value of 'playerAttack' from the value of 'enemyHealth'. Use result to update the value in the 'enemyHealth variable
-    enemyHealth = enemyHealth - playerAttack; // enemyHealth now 50 - 10 = 40
-
-    //Log a resulting message to the console so we know that it worked.
-    console.log(
-        playerName + " has attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health points."
-    );
-    //check enemyName's health
-    if (enemyHealth <= 0){  //if enemy health is less than or equal to 0, alert that the enemy has died
-        window.alert(enemyName + " has died!");
-        console.log(enemyName + " is ded");
-    }
-    else { // if enemyHealth is not less than or equal to 0, window alert the enemy's health points
-        window.alert(enemyName + " still has " + enemyHealth + " health points.");
-        console.log(enemyName + " has not ded yet!");
-    }
-    //subtract the value of 'enemyAttack' from the value of 'playerHealth'. Use that result to update the value in the 'playerHealth' var
-    playerHealth = playerHealth - enemyAttack; // playerHealth 100 - 12 = 88
-
-    //Log a resulting message to the console so we know it worked. 
-    console.log(
-        playerName + " was attacked by " + enemyName + ". " + playerName + " now has " + playerHealth + " health points."
-    );
-    //Check playerName's health
-    if (playerHealth <= 0) {
-        window.alert(playerName + " has died!");
-    }
-    else {
-        window.alert(playerName + " still has " + playerHealth + " health points left!");
     }
 }
 
